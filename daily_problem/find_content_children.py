62679a315320dfb02e815edc7c0c9ad8986ce254
omarkham <livia.markham@gmail.com>
2023-12-31 23:41:12 -0500

    2023-12-31

--- daily_problem/find_content_children.py
+++ daily_problem/find_content_children.py
@@ -4,14 +4,20 @@ class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
-        l = len(g) - 1
-        m = len(s) - 1
-        count = 0
-        while l>-1 and m>-1:
-            if g[l]<=s[m]:
-                l-=1
-                m-=1
-                count+=1
+
+        child_index, cookie_index = 0, 0
+        content_children = 0
+
+        # Iterate through both lists
+        while child_index < len(g) and cookie_index < len(s):
+            # if cookie size is good for current child
+            if s[cookie_index] >= g[child_index]:
+                # assign and move to next child
+                content_children += 1
+                child_index += 1
+                cookie_index += 1
             else:
-                l-=1
-        return count
+                # if it's not good, move to the next one
+                cookie_index += 1
+
+        return content_children
